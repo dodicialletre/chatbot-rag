fc0430a230fe537105a634a3491eb8af4e624e1f
Claudio Nuzzo <c.nuzzo@BENELLI.LOCAL>
2025-05-19 16:31:03 +0200

    Fix Flask port for Render deployment

--- app.py
+++ app.py
@@ -95,4 +95,6 @@ def ask():
     return render_template("index.html", question=query, answer=answer, namespaces=namespaces, selected=model)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    #app.run(debug=True)
+	port = int(os.environ.get("PORT", 5000))
+	app.run(host="0.0.0.0", port=port)
